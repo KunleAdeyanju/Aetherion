8b2c270021c774b55e2d9581a4cc20ba96f1fd74
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-04-21 14:52:04 -0400

    starting adding sql functionality

--- Aetherion.py
+++ Aetherion.py
@@ -1,8 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
-class Aetherion:
+class Aetherios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    type1 = db.Column(db.String(64))
+    type2 = db.Column(db.String(64))
+    type3 = db.Column(db.String(64))
+    species = db.Column(db.String(64))
+    element = db.Column(db.String(64))
+    color_palete = db.Column(db.String(64))
+    home_region = db.Column(db.String(64))
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+    attack = db.Column(db.Integer)
+    defense = db.Column(db.Integer)
+    speed = db.Column(db.Integer)
+    #  image_location = db.Column(db.String(64))
+
     def __init__(self, name, type1, type2, type3, species, element, color_palete,  \
-                 region, height, weight, attack, defense, speed, image_location):
+                 home_region, height, weight, attack, defense, speed, image_location):
         self.name = name
         self.type1 = type1
         self.type2 = type2
@@ -10,7 +28,7 @@ class Aetherion:
         self.species = species
         self.element = element
         self.color_palete = color_palete
-        self.region = region
+        self.home_region = home_region
         self.height = height
         self.weight = weight
         self.attack = attack
@@ -34,8 +52,8 @@ class Aetherion:
     def get_color_palete(self):
         return self.color_palete
     
-    def get_region(self):
-        return self.region
+    def get_home_region(self):
+        return self.home_region
     
     def get_height(self):
         return self.height
@@ -49,5 +67,7 @@ class Aetherion:
     def get_defence(self):
         return self.speed
     
-    def get_image_location(self):
-        pass
+    # def get_image_location(self):
+    #     pass
+
+
--- ImageGen.py
+++ ImageGen.py
@@ -1,6 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 class ImageGen:
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    type1 = db.Column(db.String(64))
+    type2 = db.Column(db.String(64))
+    type3 = db.Column(db.String(64))
+    species = db.Column(db.String(64))
+    element = db.Column(db.String(64))
+    color_palete = db.Column(db.String(64))
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+
     def __init__(self, name, element, type1, type2, type3, species, color_palette, height, weight):
         self.name = name
         self.species = species
