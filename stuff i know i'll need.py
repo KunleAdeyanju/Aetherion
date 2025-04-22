6c7e2736434076727753d2089e1d38aef741f8f3
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-04-22 18:51:52 -0400

    starting to write stuff

---
+++ stuff i know i'll need.py
@@ -0,0 +1,45 @@
+
+FLAME_NAME = [ 'Pyro', 'Flame', 'Scorch', 'Fire', 'Burn', 'Ignite', 'Blaze', 'Ember', 'Flare' \
+              , 'Kindle', 'Smolder', 'Ash', 'Cinder', 'Inferno', 'Singe'\
+                , 'Conflagration', 'Combustion', 'Incendiary', 'Firestorm' \
+                    , 'Wildfire', 'Backfire', 'Flashover', 'Fireball' \
+                        , 'Firebrand', 'Firecracker', 'Firecracker', 'Firestarter' ]
+
+AQUA_NAME = [ 'Aqua', 'Water', 'Wave', 'Splash', 'Tide', 'Current', 'Stream', 'Flow' \
+                , 'Surge', 'Cascade', 'Geyser', 'Fountain', 'Flood', 'Mist', 'Spray' \
+                    , 'Drizzle', 'Dew', 'Rain', 'Ice', 'Snow', 'Hail', 'Frost' \
+                        , 'Glacier', 'Tsunami', 'Torrent', 'Tempest', 'Monsoon' \
+                            , 'Cyclone', 'Whirlpool', 'Tsunami', 'Typhoon', 'Hurricane' \
+                                , 'Blizzard', 'Avalanche', 'Deluge', 'Sleet', 'Drift' \
+                                    , 'Riptide', 'Tidepool', 'Coral', 'Reef', 'Lagoon' \
+                                        , 'Estuary', 'Bay', 'Cove', 'Harbor', 'Gulf' \]
+
+EARTH_NAME = [ 'Earth', 'Rock', 'Stone', 'Boulder', 'Pebble', 'Gravel', 'Clay' \
+                , 'Soil', 'Dirt', 'Sand', 'Mud', 'Dust', 'Gravel', 'Slate' \
+                    , 'Granite', 'Marble', 'Basalt', 'Limestone', 'Quartz' \
+                        , 'Obsidian', 'Shale', 'Claystone', 'Sandstone', 'Siltstone' \
+                            , 'Gneiss', 'Schist', 'Phyllite', 'Slate', 'Conglomerate' \
+                                , 'Breccia', 'Tuff', 'Pumice', 'Scoria', 'Ash' \
+                                    , 'Tuffaceous', 'Volcanic', 'Igneous', 'Sedimentary' \
+                                        , 'Metamorphic' ]
+
+THUNDER_NAME = [ 'Thunder', 'Lightning', 'Storm', 'Bolt', 'Flash', 'Strike' \
+                    , 'Shock', 'Surge', 'Charge', 'Zap', 'Jolt', 'Boom' \
+                        , 'Crash', 'Clap', 'Rumble', 'Roar', 'Growl', 'Grumble' \
+                            , 'Thud', 'Thump', 'Pound', 'Bam', 'Bang', 'Wham' \
+                                , 'Zap', 'Zing', 'Zap', 'Zing', 'Zing' \
+                                    , 'Buzz', 'Whir', 'Whizz', 'Vroom' ]
+
+POISION_NAME = [ 'Poison', 'Toxin', 'Venom', 'Taint', 'Corruption', 'Blight' \
+                    , 'Decay', 'Rot', 'Mold', 'Mildew', 'Fungus', 'Bacteria' \
+                        , 'Virus', 'Parasite', 'Infection', 'Contagion', 'Plague' \
+                            , 'Pestilence', 'Scourge', 'Blight', 'Wasting', 'Deterioration' \
+                                , 'Decomposition', 'Putrefaction', 'Necrosis' \
+                                    , 'Gangrene', 'Sepsis', 'Mortification' ]
+
+AURA_NAME = [ 'Aura', 'Halo', 'Glow', 'Radiance', 'Luster', 'Shimmer' \
+                , 'Gleam', 'Glimmer', 'Twinkle', 'Sparkle', 'Dazzle' \
+                    , 'Glint', 'Flash', 'Flare', 'Beam', 'Ray', 'Shine' \
+                        , 'Lustre', 'Brilliance', 'Incandescence', 'Luminescence' \
+                            , 'Phosphorescence', 'Fluorescence', 'Iridescence' \
+                                , 'Opalescence', 'Chiaroscuro', 'Tenebrism', 'Sfumato', 'Contrapposto' ]
