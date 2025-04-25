8f3776c9da6ed74b5171b2f7c4a43e8c64b5ae5e
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-04-25 14:24:54 -0400

    fixed bit

--- aetherios/routes.py
+++ aetherios/routes.py
@@ -54,3 +54,8 @@ def init_routes(app):
     def get_atherios():
         atherios = Aetherios.query.all()
         return jsonify([a.to_dict() for a in atherios])
+    
+    @app.route('/a', methods=['GET'])
+    def get_atherios():
+        atherios = Aetherios.query.all()
+        return jsonify([a.to_dict() for a in atherios])
